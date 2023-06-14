fdf97d77552bfccaf26fa0261bc202b86aaf8990
Nikola <n.srbulovic@hotmail.com>
2023-06-14 16:55:44 +0200

    zadatak 2,3

--- src/components/AppCars.jsx
+++ src/components/AppCars.jsx
@@ -1,2 +1,73 @@
-const AppCars = () => {};
+import { useEffect, useState } from "react";
+import { deleteCar, getCars, postCars } from "../service/carsService";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setCars, removeCar } from "../store/cars/slice";
+import selectCarsValue from "../store/cars/selector";
+
+const AppCars = () => {
+  const dispatch = useDispatch();
+  let cars = useSelector(selectCarsValue);
+
+  useEffect(() => {
+    getCars().then(({ data }) => dispatch(setCars(data.data)));
+  }, []);
+
+  const isAutomatic = (automatic) => {
+    if (automatic) {
+      return "Yes";
+    }
+    return "No";
+  };
+
+  const deleteACar = (id) => {
+    deleteCar(id);
+    dispatch(removeCar(id));
+  };
+
+  return (
+    <table className="table container mt-5">
+      <thead>
+        <tr>
+          <th>Brand</th>
+          <th>Model</th>
+          <th>Year</th>
+          <th>Max Speed</th>
+          <th>Automatic</th>
+          <th>Engine</th>
+          <th>Number of Doors</th>
+          <th>Edit</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        {cars.map((car, index) => (
+          <tr key={index}>
+            <td>{car.brand}</td>
+            <td>{car.model}</td>
+            <td>{car.year}</td>
+            <td>{car.maxSpeed}</td>
+            <td>{isAutomatic(car.isAutomatic)}</td>
+            <td>{car.engine}</td>
+            <td>{car.numberOfDoors}</td>
+            <td>
+              <Link className="btn btn-primary" to={`/edit/${car.id}`}>
+                Select
+              </Link>
+            </td>
+            <td>
+              <button
+                className="btn btn-primary"
+                onClick={() => deleteACar(car.id)}
+              >
+                -
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
 export default AppCars;
---
+++ src/service/carService.js
@@ -0,0 +1,54 @@
+import { API } from "../shared/api";
+
+export const getCars = () => {
+  return API.get("/cars");
+};
+
+export const getCar = (id) => {
+  return API.get(`/cars/${id}`);
+};
+
+export const deleteCar = (id) => {
+  return API.delete(`/cars/${id}`);
+};
+
+export const editCars = (
+  brand,
+  model,
+  year,
+  max_speed,
+  is_automatic,
+  engine,
+  number_of_doors,
+  index
+) => {
+  return API.put(`/cars/${index}`, {
+    brand,
+    model,
+    year,
+    max_speed,
+    is_automatic,
+    engine,
+    number_of_doors,
+  });
+};
+
+export const postCars = (
+  brand,
+  model,
+  year,
+  max_speed,
+  is_automatic,
+  engine,
+  number_of_doors
+) => {
+  return API.post("/cars", {
+    brand,
+    model,
+    year,
+    max_speed,
+    is_automatic,
+    engine,
+    number_of_doors,
+  });
+};
